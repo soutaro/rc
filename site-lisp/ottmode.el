934d5ca214d54689a60ef3ee0f0bc8f529ee6048
Soutaro Matsumoto <matsumoto@soutaro.com>
2009-10-22 19:32:55 +0900

    site-lisp

---
+++ site-lisp/ottmode.el
@@ -0,0 +1,31 @@
+(define-generic-mode 'ott-mode
+  '("%")
+  '("metavar" "indexvar" "grammar" "embed" "subrules" "contextrules" "substitutions" "single" "multiple" "freevars" "defns" "defn" "by" "homs" "funs" "fun" "parsing")
+  '(
+;    ("\\[\\[.+\\]\\]" . 'font-lock-warning-face)
+;    ("\\[\\[[^\\]]+\\]\\]" . 'font-lock-warning-face)
+    ("{{\\([^{}]+{[^{}]*}\\)*[^{}]*}}" . 'font-lock-doc-face)
+;    ("{{[^}]+}}" . 'font-lock-doc-face)
+    ("(\\+.+\\+)" . 'font-lock-keyword-face)
+    ("<<.*" . 'font-lock-keyword-face)
+    (">>" . 'font-lock-keyword-face)
+    ("</" . 'font-lock-keyword-face)
+    ("//" . 'font-lock-keyword-face)
+    ("IN" . 'font-lock-keyword-face)
+    ("/>" . 'font-lock-keyword-face)
+    (" | " . 'font-lock-keyword-face)
+    (";" . 'font-lock-keyword-face)
+    ("<::" . 'font-lock-keyword-face)
+    ("_::" . 'font-lock-keyword-face)
+    ("::=" . 'font-lock-keyword-face)
+    ("::" . 'font-lock-keyword-face)
+    )
+    (list "\\.ott\\'")
+;  '(".keymap\\'" ".map\\'")
+  nil
+  "Major mode for editing ott format files.")
+
+
+;    ("\\%.*" . 'font-lock-doc-face)
+
+(provide 'ottmode)
